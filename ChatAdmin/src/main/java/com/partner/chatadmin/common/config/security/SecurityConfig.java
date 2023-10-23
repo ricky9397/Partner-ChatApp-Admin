1799e1793cd5ac36b5ad2639a0edeefad6b083c2
ricky <kimzzan1234@gmail.com>
2023-10-23 13:15:39 +0900

    commit

---
+++ ChatAdmin/src/main/java/com/partner/chatadmin/common/config/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.partner.chatadmin.common.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userSecurityService).passwordEncoder()
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+}
